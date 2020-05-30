5889ec5c11b97a74fbdc240e4ef7543318a4b015
samet <osmansamet58@gmail.com>
2020-05-30 17:45:17 +0300

    changes

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 $(document).ready(function(){
     var arena = $("#arena");
-    var player1Dom = $("#player1");
-    var player2Dom = $("#player2");
+    var playerDom = $("#player");
+    var computerDom = $("#computer");
     var ballDom = $("#ball");
+    var userScore = $("#user-score");
+    var computerScore = $("#computer-score");
+    var gameOver = $("#gameover");
 
     var Ball = function(element){
         this.x = arena.innerWidth() / 2 - ballDom.innerWidth();
         this.y = arena.innerHeight() / 2 - ballDom.innerHeight();
-        this.xSpeed = 1;
-        this.ySpeed = 3;
+        this.velocityX = 5;
+        this.velocityY = 5;
+        this.speed = 1.3;
         this.radius = 25;
         this.update = function(){
-            this.x = this.x + this.xSpeed;
-            this.y = this.y + this.ySpeed;
+            this.x = this.x + this.velocityX * this.speed ;
+            this.y = this.y + this.velocityY * this.speed;
         }
         this.reset = function(){
             this.x = arena.innerWidth() / 2 - ballDom.innerWidth();
             this.y = arena.innerHeight() / 2 - ballDom.innerHeight();
+            ball.velocityX = -this.velocityX;
+            this.speed = 1.3;
         }
         this.edges = function(){
             if(this.y < 0 || this.y > arena.innerHeight() - this.radius){
-                this.ySpeed *= -1;
-            }
-            if(this.x > arena.innerWidth() - this.radius){
-                this.xSpeed *= -1;
-            }
-            if(this.x < 0){
-                this.xSpeed *= -1;
+                this.velocityY *= -1;
+                this.speed += 0.05;
             }
         }
         this.drawBall = function(){
@@ -42,50 +43,108 @@ $(document).ready(function(){
         }
     }
 
-    var Player = function(element, name){
-        this.name = name;
-        this.top = 0;
-        this.maximumTop = arena.innerHeight() - element.innerHeight();
-        this.minimumTop = 0;
-        this.moveDown = function(){
-            if(this.top < this.maximumTop){
-                element.css({ top: this.top + 10 });
-                this.top = this.top + 10;
-            }
-        }
-        this.moveUp = function(){
-            if(this.top > this.minimumTop){
-                element.css({ top: this.top - 10 });
-                this.top = this.top - 10;
-            }
+    var Computer = function(element){
+        this.x = arena.innerWidth() - element.innerWidth();
+        this.y = (arena.innerHeight() / 2) - (element.innerHeight() / 2);
+        this.score = 0;
+        this.draw = function(){
+            element.css({
+                top: this.y,
+                left: this.x,
+        });
         }
     }
 
-    var playerOne = new Player(player1Dom);
-    var playerTwo = new Player(player2Dom);
+    var Player = function(element, name){
+        this.name = name;
+        this.x = 0;
+        this.y = (arena.innerHeight() / 2) - (element.innerHeight() / 2);
+        this.score = 0;
+        this.draw = function(){
+            element.css({
+                top: this.y,    
+                left: this.x
+            });
+        }
+    }
 
+    var Collision = function(ball, player){
+        this.check = function(){
+        this.ball = ball;
+        this.player = player;
+        this.player.top = this.player.y;
+        this.player.bottom = this.player.y + 100;
+        this.player.left = this.player.x;
+        this.player.right = this.player.x + 15;
+        
+        this.ball.top = this.ball.y - this.ball.radius;
+        this.ball.bottom = this.ball.y + this.ball.radius;
+        this.ball.left = this.ball.x - this.ball.radius;
+        this.ball.right = this.ball.x + this.ball.radius;
+        return this.ball.right > this.player.left && this.ball.top < this.player.bottom && this.ball.left < this.player.right && this.ball.bottom > this.player.top;
+        }
+    }
+
+    var player = new Player(playerDom);
+    var computer = new Computer(computerDom);
     var ball = new Ball(ballDom);
 
-   // ball.drawBall();
-   console.log(arena.innerWidth());
-    setInterval(function(){
+    computer.draw();
+    player.draw();
+
+    var gameLoop = setInterval(function(){
+        if(player.score === 3){
+            clearInterval(gameLoop);
+            $(document).off();
+            playerDom.hide();
+            computerDom.hide();
+            gameOver.css({
+                display: "flex"
+            });
+            gameOver.html("oyun bitti kazanan : player");
+        }
+        if(computer.score === 3){
+            clearInterval(gameLoop);
+            $(document).off();
+            playerDom.hide();
+            computerDom.hide();
+            gameOver.css({
+                display: "flex"
+            });
+            gameOver.html("oyun bitti kazanan : bilgisayar");
+        }
+        if(ball.x - ball.radius < 0){
+            computer.score++;
+            computerScore.html(computer.score);
+            ball.reset();
+        }else if(ball.x + ball.radius > arena.innerWidth()){
+            player.score++;
+            userScore.html(player.score);
+            ball.reset();
+        }else{
+        
+        }
+        computer.y += (ball.y - (computer.y + (computerDom.innerHeight() / 2 ))) * 0.2;
+        computer.draw();
+        let user = (ball.x < arena.innerWidth()/2 ) ? player : computer;
+        var col = new Collision(ball, user);
+        if(col.check()){
+            let collidePoint = (ball.y - (player.y + 100/2));
+            collidePoint = collidePoint / (100 / 2);
+            let angleRad = (Math.PI / 4) * collidePoint;
+            let direction = (ball.x < arena.innerWidth() / 2) ? 1 : -1;
+            ball.velocityX =  direction * ball.speed * Math.cos(angleRad);
+            ball.velocityY = ball.speed * Math.sin(angleRad);
+            ball.speed = ball.speed + 0.2;
+        }
         ball.update();
         ball.edges();
         ball.drawBall();
     },10);
 
-
-    $(document).on('keydown', function(e){
-        switch(e.which) {
-            case 87:
-                playerOne.moveUp();
-                break;
-            case 83:
-                playerOne.moveDown();
-              break;
-            default:
-              console.log("boş");
-          }
-    })
+    $(document).on('mousemove', function(e){
+        player.y = e.clientY - arena.innerHeight() + playerDom.innerHeight();
+        player.draw();
+    });
 
 });
